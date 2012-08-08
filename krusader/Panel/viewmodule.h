03bd88f4e1b14fbeee99da6b79e8630d5dbef6fa
Jan Lepper <jan_lepper@gmx.de>
2012-08-08 15:19:38 +0200

    fix wrong copyright date (the files were created in 2012 not 2011)

--- krusader/Panel/viewmodule.h
+++ krusader/Panel/viewmodule.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) 2011 Jan Lepper <jan_lepper@gmx.de>                         *
+ * Copyright (C) 2012 Jan Lepper <jan_lepper@gmx.de>                         *
  *                                                                           *
  * This program is free software; you can redistribute it and/or modify      *
  * it under the terms of the GNU General Public License as published by      *
